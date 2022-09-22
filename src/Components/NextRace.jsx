22ce6fa0d012ead880afacae0c9f3e6856f196eb
sandrop <sandrodanpozar@gmail.com>
2022-09-22 03:52:10 +0200

    Rounded flag

--- src/Components/NextRace.jsx
+++ src/Components/NextRace.jsx
@@ -67,7 +67,7 @@ function NextRace() {
 
           <div style={{display:"inline-block",textAlign:"center",width:"300px" ,margin:"0 auto"}}>
             <div style={{border:"black 2px solid", margin:"0 auto"}}>
-            <Image src={parsedRaceList[0].CountryflagURL} style={{}}></Image>
+            <Image src={parsedRaceList[0].CountryflagURL} style={{}} radius="lg"></Image>
             </div>
             <Text color="red" weight={800} size="xl">
               {parsedRaceList[0].RaceName._text} <br></br>{" "}
